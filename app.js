a72289ba4940599d3da67025f6d90a096163c03c
Jason Sykes <jasonsykes@dreamfactory.com>
2013-09-24 11:17:40 -0400

    formatting, removing docs

--- app.js
+++ app.js
@@ -22,9 +22,8 @@ window.app.getTables = function () {
     });
 };
 //get records from a table?  easy.  Just pass the path variable table_name
-window.app.getTodos = function(){
-    window.df.apis.db.getRecords({table_name:"todo"}, function(response)
-    {
-       console.log(response);
+window.app.getTodos = function () {
+    window.df.apis.db.getRecords({table_name: "todo"}, function (response) {
+        console.log(response);
     });
 }
